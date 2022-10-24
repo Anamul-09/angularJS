fd48ec7b2a606e6c2ab0eef3bdf6b466340cf3e8
Anamul-09 <mdanamulislam09@gmail.com>
2022-10-24 18:54:49 +0600

    first commit

---
+++ Primer/practices/example.js
@@ -0,0 +1,13 @@
+// function myFunction(param) {
+//     let result = param + 100;
+//     console.log("My result: " + result);
+//    }
+//    myFunction(1);
+//    myFunction("London");
+
+   function myFunction(param){
+    let result = param + 100;
+    console.log("My result : " + result);
+   }
+
+   myFunction('anam');
