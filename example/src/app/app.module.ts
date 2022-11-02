bb4073ac61c57d0dd4065a4a6bc52dc0360ef709
Anamul-09 <mdanamulislam09@gmail.com>
2022-11-02 19:09:10 +0600

    2oct2022

--- example/src/app/app.module.ts
+++ example/src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// import{RouterModule,Routes} from '@angular/router'
+
 import { AppComponent } from './app.component';
 import { headerComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
